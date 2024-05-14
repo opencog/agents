9e1161206e2ae9ea279d789e2e0acd163244a61d
Linas Vepštas <linasvepstas@gmail.com>
2024-05-13 23:38:06 -0500

    Work on fishing things out of the atomspace

--- scm/pair-agent/generate-agent.scm
+++ scm/pair-agent/generate-agent.scm
@@ -13,5 +13,40 @@
 (use-modules (opencog sensory))
 (use-modules (srfi srfi-1))
 
+; How many edges?
+(cog-incoming-size (Bond "ANY"))
+
+; Find next word after WORD
+(define (make-next-word-qry WORD)
+	(Meet (Variable "next") ; vardecl
+		(EdgeLink (Bond "ANY")
+			(List WORD (Variable "next")))))
+
+; check it out
+(cog-incoming-size (Word "start"))
+(cog-incoming-size-by-type (Word "start") 'List)
+
+(define next-list
+	(cog-execute! (make-next-word-qry (Word "start"))))
+
+(length (cog-value->list next-list))
+
+; The MI sits on the edges, so repeat the above, with edges.
+(define (make-next-edge-qry WORD)
+	(Query (Variable "next") ; vardecl
+		(EdgeLink (Bond "ANY") (List WORD (Variable "next"))) ; pattern
+		(EdgeLink (Bond "ANY") (List WORD (Variable "next"))) ; output
+	))
+
+; Look at it.
+(define next-edges
+	(cog-execute! (make-next-edge-qry (Word "start"))))
+
+(length (cog-value->list next-edges))
+(cog-keys (car (cog-value->list next-edges)))
+
+(cog-value (car (cog-value->list next-edges)) (Predicate "*-Mutual Info Key-*"))
+
+
 
 

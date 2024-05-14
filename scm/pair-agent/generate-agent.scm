75e9992362595f082b1baf1ee9286f5b2e2ae7d4
Linas Vepštas <linasvepstas@gmail.com>
2024-05-14 00:01:18 -0500

    Filter out the MI value

--- scm/pair-agent/generate-agent.scm
+++ scm/pair-agent/generate-agent.scm
@@ -47,6 +47,38 @@
 
 (cog-value (car (cog-value->list next-edges)) (Predicate "*-Mutual Info Key-*"))
 
+; Run the query, pull off the MI
+(define (make-mipr-list START)
+	(Filter
+		(Rule
+			(Variable "edge")
+			(Variable "edge")
+			(ValueOf (Variable "edge") (Predicate "*-Mutual Info Key-*")))
+		(make-next-edge-qry START)))
 
+(define next-mi
+	(cog-execute! (make-mipr-list (Word "start"))))
+
+(length (cog-value->list next-mi))
+(car (cog-value->list next-mi))
+
+; The MI is the second item. Use Decimate to pull it out.
+(define (make-mi-list START)
+	(Filter
+		(Rule
+			; Some pairs didn't have a valid MI on them! Why ???
+			(TypedVariable (Variable "mi-pair") (Type 'FloatValue))
+			(Variable "mi-pair")
+			(Decimate (Number 0 1) (Variable "mi-pair")))
+		(make-mipr-list START)))
+
+(define next-mi
+	(cog-execute! (make-mi-list (Word "start"))))
+
+(length (cog-value->list next-mi))
+(car (cog-value->list next-mi))
+
+; Fails due to typechecking
+(cog-execute! (Accumulate (make-mi-list (Word "start"))))
 
 

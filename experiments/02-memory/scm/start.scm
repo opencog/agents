42c42eaba3801467bb5185faa1731e41ec6cd7b1
Linas Vepštas <linasvepstas@gmail.com>
2026-02-25 06:09:31 +0000

    Add stub to actually run the pipeline

--- experiments/02-memory/scm/start.scm
+++ experiments/02-memory/scm/start.scm
@@ -4,7 +4,27 @@
 ;
 ; start.scm -- startup script
 ;
-(use-modules (opencog) (opencog sensory) (opencog cogserver))
+; Opens the installed RocksDB memory, loads all Atomese definitions
+; back into the AtomSpace, then runs the directory-scanning pipeline.
+;
+(use-modules (opencog) (opencog sensory))
 (use-modules (opencog persist) (opencog persist-rocks))
 
-; Placeholder -- nothing here yet.
+; ---------------------------------------------------------------
+; Open the RocksDB and load all stored atoms.
+;
+(define rsn (RocksStorageNode
+	"rocks:///usr/local/share/atomese/memory"))
+(cog-open rsn)
+(load-atomspace)
+(cog-close rsn)
+
+; ---------------------------------------------------------------
+; Run the file-scanning pipeline.
+;
+; First open the directory handles and the log file, then scan
+; both directories.  Results are cached at (Anchor "file-pipe")
+; and logged to /tmp/agent.log.
+;
+(Trigger (Name "dir-open"))
+(Trigger (Name "file-scan"))
